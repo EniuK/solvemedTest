641c8600271fdc96232c6e98d6e1cec28a584e70
Anastasiia Tripuz <anastasiia.tripuz@gmail.com>
2022-10-11 16:09:28 +0200

    Fixed images rendering in Safari

--- src/components/BackgroundImage/BackgroundImage.tsx
+++ src/components/BackgroundImage/BackgroundImage.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 import { isMobileSafari, isSafari } from "react-device-detect";
 import styles from "./BackgroundImage.module.css";
 
 type Props = { src: string; alt: string; className?: string };
 
-const BackgroundImage: React.FC<Props> = ({ src, alt, className = "" }) => (
-  <img src={src} alt={alt} className={`${styles.img} ${isSafari || isMobileSafari ? styles.bgImageSafari : ""} ${className}`} />
-);
+const BackgroundImage: React.FC<Props> = ({ src, alt, className = "" }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    // Workaround for React hydration error in Safari
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+
+  return <img src={src} alt={alt} className={`${styles.img} ${isSafari || isMobileSafari ? styles.bgImageSafari : ""} ${className}`} />;
+};
 
 export default BackgroundImage;

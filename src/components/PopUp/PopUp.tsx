64da74fd57c5ff54c8d4527a971f4e77653d9fe6
EniuK <eniuk113@gmail.com>
2023-04-25 08:07:26 +0200

    fix popup

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -1,6 +1,7 @@
-import { Box, Link, Typography, Input, Button, FormControl } from "@mui/material";
+import { Box, Typography, Input, Button, useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
+import { theme } from "../../config/theme";
 import PopUpWrapper from "./PopUpWrapper";
 
 const PopUp = ({ onClose, status, message, onValidated }: any) => {
@@ -10,6 +11,8 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
   const [inputClick, setInputClick] = useState(false);
   const [clickState, setClickState] = useState(false);
   const boxRef = useRef(null);
+  const isSmallViewport = useMediaQuery(theme.breakpoints.down("sm"));
+
   const [windowDimensions, setWindowDimensions] = useState({
     width: 0,
     height: 0,
@@ -43,45 +46,61 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
     setInputClick(true);
   };
   const outerHandler = () => {
-    let help = inner;
-    let help2 = inputClick;
-    let help3 = clickState;
-
-    setOuter(true);
-
-    if (!help) {
-      if (!help2) {
-        if (!help3) onClose();
-      } else {
-        setInner(true);
-        help = true;
-        setInputClick(true);
-        help2 = true;
-      }
-    } else {
-      setInner(true);
-      help = true;
-      setInputClick(true);
-      help2 = true;
-    }
+    // let help = inner;
+    // let help2 = inputClick;
+    // let help3 = clickState;
+    // setOuter(true);
+    // if (!help) {
+    //   if (!help2) {
+    //     if (!help3) onClose();
+    //   } else {
+    //     setInner(true);
+    //     help = true;
+    //     setInputClick(true);
+    //     help2 = true;
+    //   }
+    // } else {
+    //   setInner(true);
+    //   help = true;
+    //   setInputClick(true);
+    //   help2 = true;
+    // }
   };
 
   // mailchimp
 
   const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     email &&
       email.indexOf("@") > -1 &&
-      onValidated({
+      (await onValidated({
         MERGE0: email,
-      });
-    await console.log(status);
-    await console.log(message);
-    // onClose();
+      }));
+
+    if (email.indexOf("@") === -1) {
+      setErrorMessage(true);
+    } else {
+      setErrorMessage(false);
+    }
   };
+
+  const handleEmail = (e: any) => {
+    setEmail(e.target.value);
+    setErrorMessage(false);
+  };
+
+  useEffect(() => {
+    if (status === "error" || status === "sending") {
+      setErrorMessage(true);
+    } else {
+      setEmail("");
+    }
+  }, [status]);
+
   return (
     <PopUpWrapper onclick={outerHandler}>
       <Box
@@ -91,7 +110,7 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
         border={"thin"}
         boxShadow={"0px 8px 32px rgba(27, 37, 74, 0.08)"}
         position={"fixed"}
-        width={windowDimensions.width < 480 ? 335 : 480}
+        width={isSmallViewport ? 335 : 480}
         minHeight={357}
         bgcolor={"rgba(245, 245, 247, 1)"}
         borderRadius={6}
@@ -126,7 +145,15 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
         }}
       >
         <Box width={"100%"} display={"flex"} height={"5%"} justifyContent={"flex-end"} alignItems={"center"}>
-          <Box mr={3}>
+          <Box
+            mr={3}
+            sx={{
+              ":hover": {
+                opacity: 0.7,
+                cursor: "pointer",
+              },
+            }}
+          >
             <Image src="/images/icons/closeicon.png" alt="X" height="30px" width="30px" onClick={onClose} />
           </Box>
         </Box>
@@ -135,7 +162,7 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
           mr={4}
           fontFamily={"FinancierDisplay"}
           fontWeight={300}
-          fontSize={windowDimensions.width < 480 ? 32 : 44}
+          fontSize={isSmallViewport ? 32 : 44}
           justifyContent={"center"}
           variant={"h4"}
           alignItems={"center"}
@@ -147,64 +174,84 @@ const PopUp = ({ onClose, status, message, onValidated }: any) => {
         <Typography textAlign={"center"} fontFamily={"SuisseIntl"} fontSize={14} variant={"body4"} color={"#595D62"} mt={3} ml={4.5} mr={4.5}>
           There is going to be up to 1 high quality email a month.
         </Typography>
-        {windowDimensions.width > 480 ? (
-          <Box mt={8} ml={10} mb={5} width={"100%"} justifyContent={"center"} alignItems={"center"}>
-            <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
-              <Input
-                onClick={handleInput}
-                onFocus={() => {
-                  setInputClick(true);
-                  setInner(true);
-                  setClickState(true);
-                }}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                tabIndex={0}
-                style={{ minWidth: "273px", marginRight: "15px", marginLeft: "30px" }}
-                type="email"
-                placeholder="Enter your email"
-                ref={inputRef}
-                onBlur={() => {
-                  setInputClick(false);
-                  setClickState(false);
-                  setInner(false);
-                }}
-              />
-            </form>
+        {isSmallViewport ? (
+          <Box mt={8} ml={10} mb={2} width={"100%"} justifyContent={"center"} alignItems={"center"}>
+            {status === "success" ? (
+              <Box width={"100%"} ml={-4.5} color={"black"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                {message}
+              </Box>
+            ) : (
+              <>
+                <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
+                  <Input
+                    onClick={handleInput}
+                    onFocus={() => {
+                      setInputClick(true);
+                      setInner(true);
+                      setClickState(true);
+                    }}
+                    value={email}
+                    style={{ minWidth: "80%", marginRight: "15px" }}
+                    onChange={handleEmail}
+                    type="email"
+                    placeholder="Enter your email"
+                    ref={inputRef}
+                    onBlur={() => {
+                      setInputClick(false);
+                      setClickState(false);
+                    }}
+                  />
+                </form>
 
-            <Button color="secondary" sx={{ backgroundColor: "black" }} onClick={(e) => handleSubmit(e)} variant="contained" size="large">
-              Send
-            </Button>
+                <Box width={"80%"} display={"flex"} alignItems={"flex-end"} justifyContent={"flex-end"}>
+                  <Button color="secondary" sx={{ backgroundColor: "black", marginTop: "20px" }} onClick={(e) => handleSubmit(e)} variant="contained" size="large">
+                    Send
+                  </Button>
+                </Box>
+              </>
+            )}
           </Box>
         ) : (
-          <Box mt={8} ml={10} mb={2} width={"100%"} justifyContent={"center"} alignItems={"center"}>
-            <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
-              <Input
-                onClick={handleInput}
-                onFocus={() => {
-                  setInputClick(true);
-                  setInner(true);
-                  setClickState(true);
-                }}
-                style={{ minWidth: "80%", marginRight: "15px" }}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                placeholder="Enter your email"
-                ref={inputRef}
-                onBlur={() => {
-                  setInputClick(false);
-                  setClickState(false);
-                }}
-              />
-            </form>
+          <Box mt={8} ml={10} mb={5} width={"100%"} display={"flex"} justifyContent={"flex-start"} alignItems={"center"}>
+            {status === "success" ? (
+              <Box width={"100%"} ml={-4.5} color={"black"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                {message}
+              </Box>
+            ) : (
+              <>
+                <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
+                  <Input
+                    onClick={handleInput}
+                    onFocus={() => {
+                      setInputClick(true);
+                      setInner(true);
+                      setClickState(true);
+                    }}
+                    value={email}
+                    onChange={handleEmail}
+                    tabIndex={0}
+                    style={{ minWidth: "273px", marginRight: "15px", marginLeft: "30px" }}
+                    type="email"
+                    placeholder="Enter your email"
+                    ref={inputRef}
+                    onBlur={() => {
+                      setInputClick(false);
+                      setClickState(false);
+                      setInner(false);
+                    }}
+                  />
+                </form>
 
-            <Box width={"80%"} display={"flex"} alignItems={"flex-end"} justifyContent={"flex-end"}>
-              <Button color="secondary" sx={{ backgroundColor: "black", marginTop: "20px" }} onClick={(e) => handleSubmit(e)} variant="contained" size="large">
-                Send
-              </Button>
-            </Box>
+                <Button color="secondary" sx={{ backgroundColor: "black" }} onClick={(e) => handleSubmit(e)} variant="contained" size="large">
+                  Send
+                </Button>
+              </>
+            )}
           </Box>
         )}
+        {status === "sending" && <Box style={{ color: "blue" }}>sending...</Box>}
+        {status === "error" && errorMessage && <Box style={{ color: "red" }}>invalid email address</Box>}
+        {status === "error" ? null : <>{errorMessage && status !== "sending" ? <Box style={{ color: "red" }}>invalid email address</Box> : null}</>}
       </Box>
     </PopUpWrapper>
   );

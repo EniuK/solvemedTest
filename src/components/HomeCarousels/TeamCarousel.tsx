358a157bdfb33da4c6e4b57905f0440ec2d794b7
EniuK <eniuk113@gmail.com>
2023-05-31 08:37:18 +0200

    carousel fix 12

--- src/components/HomeCarousels/TeamCarousel.tsx
+++ src/components/HomeCarousels/TeamCarousel.tsx
@@ -104,12 +104,12 @@ const TeamCarousel = () => {
       data-aos-anchor-placement="center"
     >
       {isMobileView ? (
-        <Box ml={"-16px"} width={"100%"} display={"flex"}>
+        <Box ml={"-8px"} width={"100vw"} display={"flex"}>
           <Swiper slidesPerView={1.3} slidesOffsetBefore={0} slidesOffsetAfter={0} modules={[Pagination]} pagination={true} className="mySwiper">
             {teamData.map((e, idx) => {
               return (
                 <LazyLoadComponent key={idx}>
-                  <SwiperSlide style={{ width: "100%", maxWidth: "301px" }}>
+                  <SwiperSlide style={{ maxWidth: "401px" }}>
                     <Box
                       minHeight={"452px"}
                       maxWidth={"356px"}
